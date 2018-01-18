17d7ce67d4089bbce7ba6d93295058e8c785a3fd
bernard4132 <bernardlomo2@gmail.com>
2018-01-18 13:39:02 +0000

    ad changes

--- app/mailers/dream_mailer.rb
+++ app/mailers/dream_mailer.rb
@@ -1,39 +1,39 @@
 class DreamMailer < ApplicationMailer
-  default from: "HelpGhanaianBusinesses@helpghanaianbusinesses.com"
+  default from: "HelpBusinessesOnline@helpbusinessesonline.com"
 
   def dream_message(dream)
   	@dream = dream
-  	mail to: dream.dreameremail , subject: "[Help Ghanaian Businesses] A website is your First and Great Step!"
+  	mail to: dream.dreameremail , subject: "[Help Businesses Online] A website is your First and Great Step!"
   end
 
   def dream_message1(dream)
   	@dream = dream
-  	mail to: dream.dreameremail , subject: "[Help Ghanaian Businesses] Why we think you need a Website."
+  	mail to: dream.dreameremail , subject: "[Help Businesses Online] Why we think you need a Website."
   end
 
   def dream_message2(dream)
   	@dream = dream
-  	mail to: dream.dreameremail , subject: "[Help Ghanaian Businesses] See how gets clients everyday with their website."
+  	mail to: dream.dreameremail , subject: "[Help Businesses Online] See how gets clients everyday with their website."
   end
 
   def dream_message3(dream)
   	@dream = dream
-  	mail to: dream.dreameremail , subject: "[Help Ghanaian Businesses] Attracting customers to your website with your website."
+  	mail to: dream.dreameremail , subject: "[Help Businesses Online] Attracting customers to your website with your website."
   end
 
   def dream_message4(dream)
   	@dream = dream
-  	mail to: dream.dreameremail , subject: "[Help Ghanaian Businesses] How these top Businesses in Ghana are making a killing with their website. (How to do same with a blueprint)"
+  	mail to: dream.dreameremail , subject: "[Help Businesses Online] How these top Businesses in Ghana are making a killing with their website. (How to do same with a blueprint)"
   end
 
   def dream_message5(dream)
   	@dream = dream
-  	mail to: dream.dreameremail , subject: "[Help Ghanaian Businesses] 20% of all our Website Plans! You know you need a website by now.."
+  	mail to: dream.dreameremail , subject: "[Help Businesses Online] 20 Percent of all our Website Plans! You know you need a website by now.."
   end
 
   def dream_message6(dream)
     @dream = dream
-    mail to: dream.dreameremail , subject: "[Help Ghanaian Businesses] 20% of all our Website Plans! This is our last offer for you.."
+    mail to: dream.dreameremail , subject: "[Help Businesses Online] 20 Percent of all our Website Plans! This is our last offer for you.."
   end
 
 end
--- app/mailers/lead_mailer.rb
+++ app/mailers/lead_mailer.rb
@@ -1,8 +1,49 @@
 class LeadMailer < ApplicationMailer
-	default from: "HelpGhanaianBusinesses@helpghanaianbusinesses.com"
+	default from: "HelpBusinessesOnline@helpbusinessesonline.com"
 
   def lead_message(lead)
   	@lead = lead
   	mail to: lead.email , subject: "You got hitched on some Goodness!"
   end
+
+  def lead_message(lead)
+  	@lead = lead
+  	mail to: lead.email , subject: "You got hitched on some Goodness!"
+  end
+
+  def lead_message(lead)
+  	@lead = lead
+  	mail to: lead.email , subject: "You got hitched on some Goodness!"
+  end
+
+  def lead_message(lead)
+  	@lead = lead
+  	mail to: lead.email , subject: "You got hitched on some Goodness!"
+  end
+
+  def lead_message(lead)
+  	@lead = lead
+  	mail to: lead.email , subject: "You got hitched on some Goodness!"
+  end
+
+  def lead_message(lead)
+  	@lead = lead
+  	mail to: lead.email , subject: "You got hitched on some Goodness!"
+  end
+
+  def lead_message(lead)
+  	@lead = lead
+  	mail to: lead.email , subject: "You got hitched on some Goodness!"
+  end
+
+  def lead_message(lead)
+  	@lead = lead
+  	mail to: lead.email , subject: "You got hitched on some Goodness!"
+  end
+
+  def lead_message(lead)
+  	@lead = lead
+  	mail to: lead.email , subject: "You got hitched on some Goodness!"
+  end
+
 end
