9a53e04b9425393fc95ad1bd5376f49349ab21fa
bernard4132 <bernardlomo2@gmail.com>
2019-01-22 07:29:50 +0000

    new push

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -69,7 +69,6 @@ class AppointmentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def appointment_params
-      params[:appointment][:doyouhave] = params[:appointment][:doyouhave].join(",")
       params.require(:appointment).permit(:name, :companyname, :numofemployees, :usedsocmedmarket, :wasiteffective, :doyouhave, :usedanytopromote, :email, :phonenum)
     end
 end
